297b3873123b4986b93d00c9157ceb207a044b8c
Gun Lee <gdlee412@gmail.com>
2025-11-22 23:06:22 +0900

    PA 4

--- Assignment/pa4/xv6-riscv/user/swaptest.c
+++ Assignment/pa4/xv6-riscv/user/swaptest.c
@@ -1,4 +1,3 @@
-/*
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
@@ -7,43 +6,4 @@ int main () {
 	int a = -1, b = -1;
     swapstat(&a, &b);
     printf("a: %d, b: %d\n", a, b);
-}
-*/
-#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "user/user.h"
-
-void
-consumeswap()
-{
-    // Allocate enough memory to fill a significant portion of swap
-    // Adjust loop count if your swap space is very large
-    int i;
-    for(i = 0; i < 3000; i++){
-        char *p = sbrk(4096);
-        if(p == (char*)-1) break;
-        *p = 'z';
-    }
-    // Just exit immediately, triggering uvmunmap
-    exit(0);
-}
-
-int
-main()
-{
-    printf("SwapLeak Test: Running multiple heavy processes...\n");
-
-    // We run the heavy process multiple times.
-    // If swap slots are leaked, later iterations will fail.
-    for(int i = 0; i < 10; i++) {
-        printf("Iteration %d...\n", i);
-        int pid = fork();
-        if(pid == 0) {
-            consumeswap();
-        }
-        wait(0);
-    }
-
-    printf("SwapLeak Test: SUCCESS (Survived 10 iterations without OOM)\n");
-    exit(0);
 }
